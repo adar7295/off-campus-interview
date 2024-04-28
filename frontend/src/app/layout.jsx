a3330165512bdbb2ce10187e2e5e87069e4616ad
adar7295 <ssrivastav7295@gmail.com>
2024-04-28 14:01:08 +0530

    signup

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-white">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,29 +10,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors:{
-      'white' : '#ffffff',
-      'indigo' : '#6366F1',
-      'grey' : '#495057',
-      'indigo-700':'#3333cc',
-     'blue':{
-      50:'#a9d6e5',
-      100:'#89c2d9',
-      150:'#61a5c2',
-      200:'#468faf',
-      250:'#2c7da0',
-      300:'#2a6f97', 
-      350:'#014f86',
-      400:'#01497c',
-      450:'#013a63',
-      500:'#012a4a',
-     },
-    
-    extend:{
-      'black' : '#0000',
-    },
-    
-    }
+    // colors: {
+    //   'white': '#ffffff',
+    //   'indigo': '#6366F1',
+    //   'grey': '#495057',
+    //   'indigo-700': '#3333cc',
+
+    //   'blue': {
+    //     50: '#000000',
+    //     100: '#89c2d9',
+    //     150: '#61a5c2',
+    //     200: '#468faf',
+    //     250: '#2c7da0',
+    //     300: '#2a6f97',
+    //     350: '#014f86',
+    //     400: '#01497c',
+    //     450: '#013a63',
+    //     500: '#012a4a',
+    //   },
+    // }
+
+  },
+  extend: {
+
   },
   plugins: [require("daisyui")],
 }
