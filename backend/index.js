f924d61c2a2c0bba15a3c109d4c8c26c7701ac83
adar7295 <ssrivastav7295@gmail.com>
2024-05-01 17:52:34 +0530

    indexJS

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// import Express
+const express = require('express');
+const userRouter = require('./router/userRouter');
+const cors = require('cors');
+
+// initializing express
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors({
+    origin: ['http://localhost:3000']
+}))
+
+app.use(express.json());
+app.use('/user', userRouter);
