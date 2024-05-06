f25312392d42175c2e2ae1b6578f88d81e3aaf51
adar7295 <ssrivastav7295@gmail.com>
2024-05-06 21:03:19 +0530

    push

--- backend/model/interviewModel.js
+++ backend/model/interviewModel.js
@@ -2,6 +2,7 @@ const { model, Schema, Types } = require('../connection');
 
 const mySchema = ({
     company : { type : Types.ObjectId, ref : ''},
+    email:string,
     designation: String,
     location: String,
     createdAt : { type : Date, default : Date.now}
--- frontend/src/app/company/jobpost/page.jsx
+++ frontend/src/app/company/jobpost/page.jsx
@@ -3,150 +3,186 @@ import React from 'react'
 const Jobpost = () => {
   return (
     <div>
-        <>
-  {/* component */}
-  <section className="bg-white dark:bg-gray-900">
-    <div className="flex justify-center min-h-screen">
-      <div
-        className="hidden bg-cover ml-10 lg:block lg:w-2/5"
-        style={{
-          backgroundImage:
-            'url("https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80")'
-        }}
-      ></div>
-      <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
-        <div className="w-full">
-          <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
-            Get your free account now.
-          </h1>
-          <p className="mt-4 text-gray-500 dark:text-gray-400">
-            Let’s get you all set up so you can verify your personal account and
-            begin setting up your profile.
-          </p>
-          <div className="mt-6">
-            <h1 className="text-gray-500 dark:text-gray-300">
-              Select type of account
-            </h1>
-            <div className="mt-3 md:flex md:items-center md:-mx-2">
-              <button className="flex justify-center w-full px-6 py-3 text-white bg-blue-500 rounded-md md:w-auto md:mx-2 focus:outline-none">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-                <span className="mx-2">client</span>
-              </button>
-              <button className="flex justify-center w-full px-6 py-3 mt-4 text-blue-500 border border-blue-500 rounded-md md:mt-0 md:w-auto md:mx-2 dark:border-blue-400 dark:text-blue-400 focus:outline-none">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
-                <span className="mx-2">worker</span>
-              </button>
+        <div>
+      <>
+        {/* component */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html:
+              "@import url('https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css')"
+          }}
+        />
+        <div className="min-w-screen min-h-screen bg-white dark:bg-gray-900 flex items-center justify-center px-5 py-5">
+          <div
+            className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden"
+            style={{ maxWidth: 1000 }}
+          >
+            <div className="md:flex w-full">
+
+
+              <div className="hidden md:block w-1/2 bg-white py-32 px-10">
+                <img src="/p1.png" alt="" />
+              </div>
+              <div className="w-full md:w-1/2 py-10 px-5 bg-blue-250 md:px-10">
+                <div className="text-center mb-10">
+                  <h1 className="font-bold text-3xl text-black">Login</h1>
+                  <p>Enter your information to Login</p>
+                </div>
+                <div>
+                  <form >
+
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Company name
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-email-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="email"
+                            id="email"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="Enter Name of Company"
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Email
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-email-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="email"
+                            id="email"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="johnsmith@example.com"
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Designation
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-email-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="email"
+                            id="email"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="Designation"
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Number of Entries
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-email-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="email"
+                            id="email"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="johnsmith@example.com"
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Email
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-email-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="email"
+                            id="email"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="johnsmith@example.com"
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <label htmlFor="" className="text-xs font-semibold px-1">
+                          Password
+                        </label>
+                        <div className="flex">
+                          <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <i className="mdi mdi-lock-outline text-gray-400 text-lg" />
+                          </div>
+                          <input
+                            type="password"
+                            id="password"
+                           
+                            className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
+                            placeholder="************"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="flex -mx-3">
+                      <div className="w-full px-3 mb-5">
+                        <button type='submit' className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+                          LOGIN
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-          <form className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                First Name
-              </label>
-              <input
-                type="text"
-                placeholder="John"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Last name
-              </label>
-              <input
-                type="text"
-                placeholder="Snow"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Phone number
-              </label>
-              <input
-                type="text"
-                placeholder="XXX-XX-XXXX-XXX"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Email address
-              </label>
-              <input
-                type="email"
-                placeholder="johnsnow@example.com"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Password
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Confirm password
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-              <span>Sign Up </span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 rtl:-scale-x-100"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </form>
         </div>
-      </div>
-    </div>
-  </section>
-</>
+        {/* BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES */}
+        <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+          <div>
+            <a
+              title="Buy me a beer"
+              href="https://www.buymeacoffee.com/scottwindon"
+              target="_blank"
+              className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
+            >
+              <img
+                className="object-cover object-center w-full h-full rounded-full"
+                src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"
+              />
+            </a>
+          </div>
+        </div>
+      </>
 
+    </div>
     </div>
   )
 }
