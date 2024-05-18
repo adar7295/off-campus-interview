a75eadcbc034c10f058e35e0c1cd2e4236d3dbb2
adar7295 <ssrivastav7295@gmail.com>
2024-05-18 18:32:44 +0530

    fsfsd

--- frontend/src/app/company/jobpost/page.jsx
+++ frontend/src/app/company/jobpost/page.jsx
@@ -105,13 +105,15 @@ const Jobpost = () => {
                                 <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
                                   <i className="mdi mdi-email-outline text-gray-400 text-lg" />
                                 </div>
-                                <input
+                                <textarea
                                   type="text"
                                   id="jobDescription"
+                                  rows={4}
                                   onChange={jobPostForm.handleChange}
                                   values={jobPostForm.values.jobDescription}
                                   className="bg-white w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo"
                                   placeholder="Add Some Details..."
+                                  defaultValue={''}
                                 />
                                
                                 {
