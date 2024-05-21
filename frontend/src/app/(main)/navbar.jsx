6e94d3aa3a23a0692afa2b52b66057346618d875
adar7295 <ssrivastav7295@gmail.com>
2024-05-21 14:05:47 +0530

    rds

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -64,16 +65,18 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/compLogin"
                     className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
+               
                     Login
+                   
                   </a>
                   
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/compSignup"
                     className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Signup
