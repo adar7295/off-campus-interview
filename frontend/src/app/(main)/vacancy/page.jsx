902feccc6d5085bfa9336fd72e8a5309cf011c04
adar7295 <ssrivastav7295@gmail.com>
2024-06-05 15:47:50 +0530

    ljhg

--- frontend/src/app/(main)/vacancy/page.jsx
+++ frontend/src/app/(main)/vacancy/page.jsx
@@ -110,6 +110,7 @@ const Vacancy = () => {
   </div>
   <div className="flex flex-col justify-between flex-1 mt-6">
     <nav>
+    <hr className="my-6 border-gray-200 dark:border-gray-600" />
       <div>
 
   <span className="ml-2 text-black font-semibold mt-3 ">Qualification</span>
@@ -158,6 +159,7 @@ const Vacancy = () => {
           <span className="ml-2 text-gray-700">M.Com</span>
       </a>
       </div>
+      <hr className="my-6 border-gray-200 dark:border-gray-600" />
       <div className='mt-5'>
 
       <span className="ml-2 text-black font-semibold ">Salary</span>
@@ -198,54 +200,37 @@ const Vacancy = () => {
         
       </a>
             </div>
-      <a
-        className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-        href="#"
-      >
-        <svg
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <span className="mx-4 font-medium">Tickets</span>
-      </a>
-      <a
-        className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-        href="#"
-      >
-        <svg
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <span className="mx-4 font-medium">Settings</span>
-      </a>
-      <hr className="my-6 border-gray-200 dark:border-gray-600" />
+            <hr className="my-6 border-gray-200 dark:border-gray-600" />
+            <div className='mt-5'>
+
+<span className="ml-2 text-black font-semibold ">Job Type</span>
+<a
+  className="flex items-center px-4 py-2  text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+  href="#"
+>
+   <input
+      type="checkbox"
+      className="form-checkbox h-5 w-5 text-gray-600"
+      defaultChecked=""
+    />
+    <span className="ml-2 text-gray-700">Part time</span>
+  
+</a>
+<a
+  className="flex items-center px-4 py-2 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+  href="#"
+  >
+   <input
+      type="checkbox"
+      className="form-checkbox h-5 w-5 text-gray-600"
+      defaultChecked=""
+      />
+    <span className="ml-2 text-gray-700">Fulll time</span>
+  
+</a>
+      </div>
+     
+      
       <a
         className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
         href="#"
