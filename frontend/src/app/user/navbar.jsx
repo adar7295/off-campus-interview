d48142feb070c16a2f85d7da4980095eb86c22c4
adar7295 <ssrivastav7295@gmail.com>
2024-06-06 15:53:27 +0530

    vdbfd

--- frontend/src/app/user/navbar.jsx
+++ frontend/src/app/user/navbar.jsx
@@ -1,9 +1,10 @@
+'use client'
 import React from 'react'
-
-
+import Link  from 'next/link';
+import { useState } from 'react';
 
 const UserNavbar = () => {
-  return (
+return (
     <div>
       <>
         {/* component */}
@@ -40,54 +41,56 @@ const UserNavbar = () => {
             <div className="w-full md:block md:w-auto hidden" id="mobile-menu">
               <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                  <a
+                  <Link
                     href="/profile"
                     className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 text-white md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     href="/about"
                     className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     About
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    href="/vacancy"
                     className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    Services
-                  </a>
+                    Vacancy
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="/login"
-                    className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Login
-                  </a>
-                  
-                </li>
+            <Link
+              href="/login"
+              className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+  
+              Login
+  
+            </Link>
+  
+          </li>
+          <li>
+            <Link
+              href="/signup"
+              className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+              Signup
+            </Link>
+          </li>
                 <li>
-                  <a
-                    href="/signup"
-                    className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Signup
-                  </a>
-                </li>
-                <li>
-                  <a
+                  <Link
                     href="/contact"
                     className="block py-2 pr-4 pl-3 text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     Contact
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
